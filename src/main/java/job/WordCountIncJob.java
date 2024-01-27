829fc069b5a1986fd69fdd9eb76d66d742b122a2
wh <wh99602@163.com>
2024-01-27 17:04:15 +0800

    提交

---
+++ src/main/java/job/WordCountIncJob.java
@@ -0,0 +1,40 @@
+package job;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author wangheng
+ * * @date 2024/1/27
+ */
+public class WordCountIncJob {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        env
+                .socketTextStream("node4",9999)
+                .flatMap(new FlatMapFunction<String, String>() {
+                    @Override
+                    public void flatMap(String line, Collector<String> out) throws Exception {
+                        for (String word : line.split(" ")) {
+                            out.collect(word);
+                        }
+                    }
+                })
+                .map(new MapFunction<String, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(String word) throws Exception {
+                        return Tuple2.of(word, 1L);
+                    }
+                })
+                .keyBy(r->r.f0)
+                .sum(1)
+                .print();
+
+        env.execute("wordCount增量任务");
+    }
+}
